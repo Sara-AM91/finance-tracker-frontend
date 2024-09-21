a124ccb0d231da7841e3d0329c2d28e1435427a4
natakamm <natakamm1@googlemail.com>
2024-09-22 01:47:53 +0200

    made progress bar more interactive and made modal for it. Also created List component and ListItem component, but they are empty still

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import ExpensesVsIncomeBar from "./charts/ExpensesVsIncomeBar";
 import ExpensesVsIncomeLine from "./charts/ExpensesVsIncomeLine";
 import BasicPie from "./charts/BasicPie";
+import ProgressBar from "./charts/ProgressBar";
 import greenWave from "../assets/greenWave2.png";
 import redWave from "../assets/redWave.png";
+import plus from "../assets/plus.png";
 
 const Dashboard = () => {
   const [bar, setBar] = useState(true);
-  const [value, setValue] = useState(67);
+  const [balance, setBalance] = useState(14500);
+
   const [maxExp, setMaxExp] = useState({ month: "June", amount: "1600" });
   const [maxInc, setMaxInc] = useState({ month: "April", amount: "2500" });
   const toggleChart = () => {
@@ -93,35 +96,24 @@ const Dashboard = () => {
 
           {/* Progress Bar */}
           <div className="col-span-4 bg-[#161A40] p-4 text-white rounded-3xl overflow-hidden text-center">
-            <div className="flex gap-10 items-center justify-between">
-              <div className="flex flex-row items-center gap-4">
-                <h2 className="text-xl text-left">Income Goal</h2>
-                <h2 className="font-bold text-[#01FFB9] text-2xl">{value}%</h2>
-              </div>
-
-              {/* Progress bar */}
-              <div className="flex flex-col items-end w-full">
-                <p>Number/Number</p>
-                <div className="w-full bg-gray-400 h-4 rounded-r-full overflow-hidden">
-                  <div
-                    className="h-4 rounded-r-full bg-gradient-to-r from-[#F36713] to-[#7F3BCB]"
-                    style={{ width: `${value}%` }}
-                  ></div>
-                </div>
-              </div>
-            </div>
+            <ProgressBar balance={balance} />
           </div>
         </div>
 
         {/* Sidebar */}
         <div className="col-span-2 grid grid-rows-12 gap-4 h-full">
-          <div className="row-span-3 bg-[#161A40] text-white rounded-3xl overflow-auto">
-            01
+          <div className="row-span-3 bg-[#161A40] text-white rounded-3xl overflow-auto p-8">
+            <div className="flex flex-col items-end gap-2">
+              <h1 className="text-4xl">Available Balance</h1>
+              <p className="text-6xl text-[#08D59C]">{balance}</p>
+            </div>
           </div>
-          <div className="row-span-8 bg-[#161A40] text-white rounded-3xl overflow-auto">
+          <div className="row-span-8 bg-[#161A40] text-white rounded-3xl overflow-auto p-4">
             02
           </div>
-          <div className="row-span-1 bg-gradient-to-r from-purple-600 to-indigo-900 text-white rounded-3xl overflow-auto"></div>
+          <div className="row-span-1 bg-gradient-to-r from-purple-600 to-indigo-900 rounded-3xl overflow-auto flex justify-center items-center">
+            <img src={plus} className=" max-h-12 " />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/GoalModal.jsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from "react";
+
+export default function GoalModal({ setOpen, setGoal, goal }) {
+  const [number, setNumber] = useState(0);
+
+  const handleSubmit = () => {
+    if (number) {
+      setGoal(number);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <div className="relative z-10">
+      <div
+        transition
+        className="fixed inset-0 bg-blue-950 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+      />
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="">
+            <div
+              transition
+              className="relative transform overflow-hidden rounded-xl text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+            >
+              <div className="bg-[#161a40] border border-indigo-400 rounded-xl relative overflow-hidden">
+                {/* Corrected SVG Wave at the top */}
+                <div className="absolute top-0 left-0 w-full">
+                  <svg
+                    viewBox="0 0 1440 320"
+                    className="w-full h-[100px]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    preserveAspectRatio="none"
+                  >
+                    <defs>
+                      <linearGradient
+                        id="waveGradient"
+                        gradientTransform="rotate(1)"
+                      >
+                        <stop offset="0%" stopColor="#7F3BCB" />
+                        <stop offset="100%" stopColor="#633FD7" />
+                      </linearGradient>
+                    </defs>
+                    <path
+                      fill="url(#waveGradient)"
+                      fillOpacity="1"
+                      d="M0,320L48,293.3C96,267,192,213,288,202.7C384,192,480,224,576,234.7C672,245,768,235,864,218.7C960,203,1056,181,1152,186.7C1248,192,1344,224,1392,240L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+                    />
+                  </svg>
+                </div>
+
+                <div className="px-4 pb-4 pt-12 sm:p-6 sm:pb-4 relative z-10 mt-20">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <div
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-white"
+                      >
+                        Add your New Goal!
+                      </div>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          You're on the path to achieving great things! Set a
+                          new goal for your balance to help you stay focused and
+                          motivated. Remember, every step brings you closer to
+                          your success!
+                        </p>
+                        <input
+                          type="number"
+                          value={number}
+                          onChange={(e) => {
+                            setNumber(e.target.value);
+                          }}
+                          className="w-full mt-4 p-2 rounded bg-[#222759] border border-indigo-300 text-white"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="px-4 pt-3 pb-6 sm:flex sm:flex-row-reverse sm:px-6 relative z-10">
+                  <button
+                    type="button"
+                    onClick={handleSubmit}
+                    className="inline-flex w-full justify-center rounded-md bg-gradient-to-t from-orange-500 to-pink-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gradient-to-b sm:ml-3 sm:w-auto"
+                  >
+                    Set Goal
+                  </button>
+                  <button
+                    type="button"
+                    data-autofocus
+                    onClick={() => setOpen(false)}
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

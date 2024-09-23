0618c1958766be30dc1b34e37b4b1f2eb3fae436
natakamm <natakamm1@googlemail.com>
2024-09-23 17:40:23 +0200

    move MainLayout to pages

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const MainLayout = () => {
   return (

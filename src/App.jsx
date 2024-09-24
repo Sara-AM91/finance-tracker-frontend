72efc2c24b1d2e25253e246a19225ffa1f23d300
Sara-AM91 <sara.am.hafezi@gmail.com>
2024-09-24 10:35:33 +0200

    Added alert system to login page, handled declared errors on signup page, and modified App.jsx for dashboard redirection after login and signup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import MainLayout from "./components/MainLayout";
@@ -9,11 +9,17 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Redirect root path to login page */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        {/* Login and Signup routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        {/* Dashboard/Home route wrapped inside MainLayout */}
         <Route path="/home" element={<MainLayout />}>
-          <Route index element={<Dashboard />} />
+          <Route index element={<Dashboard />} /> {/* Home dashboard */}
         </Route>
+        {/* Optional: handle unknown routes by redirecting */}
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );

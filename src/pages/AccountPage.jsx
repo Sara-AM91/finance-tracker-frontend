ed5376e60c168cc6fdce9fffb0f49dd789ff0aac
natakamm <natakamm1@googlemail.com>
2024-09-25 15:16:31 +0200

    added fields for chaning information

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -7,17 +7,66 @@ const AccountPage = () => {
           Feel free to check our your account and your current account settings,
           such as Name, Profile Picture and more.
         </p>
-
-        <div class="overflow-hidden ">
-          <p class="py-2 text-xl font-semibold">Email Address</p>
-          <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-            <p class="text-gray-600">
-              Your email address is <strong>john.doe@company.com</strong>
-            </p>
-            <button class="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">
-              Change
+        <div className="bg-[#161A40] w-full  p-8 rounded-3xl shadow-lg">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <img
+                src="https://via.placeholder.com/100"
+                alt="Profile"
+                className="w-24 h-24 rounded-full border-4 border-white"
+              />
+              <div className="ml-4">
+                <p className="text-xl font-semibold">Julianne Moore</p>
+                <a href="#" className="text-blue-500 underline text-sm">
+                  View Profile
+                </a>
+              </div>
+            </div>
+            <button className="bg-gray-700 text-white py-2 px-4 rounded-lg hover:bg-gray-600">
+              Upload
             </button>
           </div>
+        </div>
+
+        <div className="mt-8 ">
+          <h2 className="text-2xl font-bold mb-4">Contact Information</h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div>
+              <label className="block text-sm mb-2">First name</label>
+              <input
+                type="text"
+                placeholder="First name"
+                className="w-full p-3 bg-gray-200 text-gray-900 rounded-md"
+              />
+            </div>
+            <div>
+              <label className="block text-sm mb-2">Last name</label>
+              <input
+                type="text"
+                placeholder="Last name"
+                className="w-full p-3 bg-gray-200 text-gray-900 rounded-md"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
+          <div>
+            <label className="block text-sm mb-2">Email</label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full p-3 bg-gray-200 text-gray-900 rounded-md"
+            />
+          </div>
+        </div>
+        <div className="mt-6 flex justify-end">
+          <button className="bg-gray-600 py-2 px-4 rounded-lg mr-4 hover:bg-gray-500">
+            Cancel
+          </button>
+          <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-400">
+            Save
+          </button>
         </div>
       </div>
 
@@ -28,56 +77,7 @@ const AccountPage = () => {
             Here you can always change and reset your password. Ensure it´s
             strong enough!
           </p>
-          <div class="flex items-center">
-            <div class="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
-              <label for="login-password">
-                <span class="text-sm text-gray-500">Current Password</span>
-                <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-                  <input
-                    type="password"
-                    id="login-password"
-                    class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
-                    placeholder="***********"
-                  />
-                </div>
-              </label>
-              <label for="login-password">
-                <span class="text-sm text-gray-500">New Password</span>
-                <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-                  <input
-                    type="password"
-                    id="login-password"
-                    class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
-                    placeholder="***********"
-                  />
-                </div>
-              </label>
-            </div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="mt-5 ml-2 h-6 w-6 cursor-pointer text-sm font-semibold text-gray-600 underline decoration-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
-              />
-            </svg>
-          </div>
-          <p class="mt-2">
-            Can't remember your current password.{" "}
-            <a
-              class="text-sm font-semibold text-blue-600 underline decoration-2"
-              href="#"
-            >
-              Recover Account
-            </a>
-          </p>
-          <button className=" mt-5 mb-2 float-end text-white text-sm py-2 px-3 bg-orange-500 rounded-lg ">
+          <button className=" mt-5 mb-2 float-end text-white py-2 px-3 bg-orange-500 rounded-lg ">
             New Password
           </button>
         </div>
@@ -87,7 +87,7 @@ const AccountPage = () => {
             Here you can delete your existing account. Keep in my mind, that
             it´s a premanent removal and can´t be retrieved.
           </p>
-          <button className=" mt-5 mb-2 float-end text-white text-sm py-2 px-3 bg-orange-500 rounded-lg ">
+          <button className=" mt-5 mb-2 float-end text-white py-2 px-3 bg-orange-500 rounded-lg ">
             Delete
           </button>
           <p class="inline-flex items-center rounded-full bg-rose-100 px-4 py-1 text-rose-600">

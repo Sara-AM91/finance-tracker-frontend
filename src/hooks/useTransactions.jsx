174123422bbedd97b0dcfbb411e879457d50e277
natakamm <natakamm1@googlemail.com>
2024-09-27 14:40:58 +0200

    added custom hooks for fetching user and transactions

---
+++ src/hooks/useTransactions.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const useTransactions = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = localStorage.getItem("token").replace(/['"]+/g, "");
+      try {
+        const response = await fetch("http://localhost:5000/transactions", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+
+        const result = await response.json();
+        setTransactions(result);
+        console.log("Transactiosn:", result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { transactions, loading, error };
+};
+
+export default useTransactions;
